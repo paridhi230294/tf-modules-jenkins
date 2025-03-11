70329a44ec2fcbb04410baa5f6b7f7c6f81d44b0
arjunachari12 <arjun.achari12@gmail.com>
2025-03-11 09:00:43 +0000

    fix bug

--- variables.tf
+++ variables.tf
@@ -16,7 +16,7 @@ variable "subnet_cidr" {
   default     = "10.0.1.0/24"
 }
 
-variable "instance_type" {`
+variable "instance_type" {
   description = "EC2 instance type"
   type        = string
   default     = "t2.micro"
