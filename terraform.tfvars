35e7d4e3673c6878760224ef55896d1788b88204
Paridhi <paridhiag.91@gmail.com>
2025-03-11 11:02:00 +0000

    keypaircommit

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,2 +1,2 @@
 aws_region = "ap-southeast-2"
-key_name   = "my-key"
+key_name   = "Paridhi2"
--- variables.tf
+++ variables.tf
@@ -25,7 +25,7 @@ variable "instance_type" {
 variable "key_name" {
   description = "Name of the SSH Key Pair"
   type        = string
-  default     = "my-key"
+  default     = "Paridhi2"
 }
 variable "rules" {
   description = "Security group rules: from_port, to_port, protocol"
